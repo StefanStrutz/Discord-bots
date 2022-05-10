fd7131e21d77f526f352f827ec99d7ec6a6605c1
Stefan X Strutz <stefanhalo@gmail.com>
2022-05-09 23:15:15 -0500

    The bot finally works. Not quite the way originally intended, but better.
    !ding ## command causes bot to say ding at certain minute.
    
    More testing needed before committing to master.

--- experiments/Time_bot.py
+++ experiments/Time_bot.py
@@ -1,17 +1,10 @@
 #This code will not run even with proper modifications. The readme includes the proper
 #modifications.
 
-#Written by Stefan X. Strutz on May 1, 2022
-#The genreal purpose of the time bought is to test interactions between the time and
-#discord module.
+#Written by Stefan X. Strutz on May 1, 2022, last updated on May 9, 2022
+#The purpose of this code is to test interactions between the time and discord module.
 
-#For my 1st attempt I was trying to make a bot that would say ding at a specific minute.
-#I discovered that the asynchronous module can only have one event loop.
-
-#It explains why a lot of functions using asynchronous module have a main function.
-#Any loop only to be contained in the discord client loop with a @client.event before
-#it. Since the discord modules based on events, I'll have to the start the timecode with 
-#a discord message
+#Changed during the function to be message triggered and set time specified in message
 
 import discord
 import time
@@ -28,7 +21,7 @@ async def on_ready():
     channel = client.get_channel(Channel_number_here)#replace 
     await channel.send('hello')
 
-#No point in a time-based discord bot if the time part breaks normal bot functions.
+# All functions must be triggered by messages as the code can only have one event loop.
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -40,23 +33,20 @@ async def on_message(message):
     if message.content.startswith('!hour'):
         await message.channel.send(time.localtime().tm_hour)
         
-async def check_time():
-    while True:
-        if time.localtime().tm_min==35:
-                channel = client.get_channel(Channel_number_here)#replace 
-                await channel.send('ding')
-        else:
-            await asyncio.sleep(24)
+    if message.content.startswith('!ding'):
+        ding_at = int(message.content[6:8])
+        not_dinged = True
+        while not_dinged:
+            channel = client.get_channel(Channel_number_here)#replace
+            if time.localtime().tm_min==ding_at:
+                    await channel.send('ding')
+                    not_dinged =False
+            else:
+                await asyncio.sleep(24)
+                #await channel.send('dong') # print debug
 
-async def launch():
-    client.run('bot_token_here') #replace 
+#Starts the event loop.
+client.run('bot_token_here') #replace
 
-#The failures are located here as both of these count as event loops.
-asyncio.run(launch())
-asyncio.run(check_time())
-    
-
-
-
-
+ 
 
